5478775f94aeb4df58f2ed0cd394ccfd2925c255
Simon Taye <mulat.simon@gmail.com>
2021-06-15 16:26:59 +0000

    Parse a repo and create a Repo struct
    OpenRepo looks for the ".git" directory and reads info from the config
    file.
    Currently, information in the Repo struct is light; only repo dir,
    worktree and branche names.
    Also added helper function for traversing the filesystem and looking
    for a repository in the parent directories.

---
+++ repo/repo.go
@@ -0,0 +1,122 @@
+package repo
+
+import ( 
+    "os"
+    "path"
+    "strings"
+    "github.com/SimonMTaye/gitgo/iniparse"
+    )
+
+
+type Repo struct {
+    repoPath string
+    worktree string
+    branches []Branch
+}
+
+type Branch struct {
+    name string
+}
+
+// Indicates that the given directory does not contain a repository
+type ErrNoRepository struct {
+    dir string
+}
+
+// Indicates that a repository could not be found in the current dir or any of its parents
+type ErrNoRepositoryFound struct {
+    dir string
+}
+
+
+func (e ErrNoRepository) Error() string {
+    return "Directory does not contain a repository: "+ e.dir
+}
+
+func (e ErrNoRepositoryFound) Error() string {
+    return "Could not find repository in the directory or its parents: " + e.dir
+}
+
+// Checks the current directory for a ".git" directory, returns an error if it is not found
+// Reads the "config" file in the ".git" directory and returns a Repo struct with the 
+// current repos properties
+func OpenRepo (dir string) (*Repo, error) {
+    // Look for the ".git" directory
+    dirs, err := os.ReadDir(dir)
+    if err != nil {
+        return nil, err
+    }
+
+    if !exists(dirs, ".git") {
+        return nil, ErrNoRepository{dir: dir}
+    }
+
+    // Open the "config" file
+    gitDir := path.Join(dir, ".git")
+    configFile, err := os.Open(path.Join(gitDir, "config"))
+    if err != nil {
+        return nil, err
+    }
+    // Parse the config file
+    configIni, err := iniparse.ParseIni(configFile)
+    if err != nil {
+        return nil, err
+    }
+
+    branches, err := getBranchesFromConfigIni(&configIni)
+    if err != nil {
+        return nil, err
+    }
+
+    repo := Repo{ repoPath: dir, branches: branches}
+
+    worktree, ok := configIni["core"]["worktree"]
+    if ok {
+        repo.worktree = worktree
+    } else {
+        repo.worktree = dir
+    }
+    return &repo, nil
+    
+}
+
+// Recursively checks parent directory until a ".git" is found or "/" is reached
+func FindRepo (cwd string) (string, error) {
+    curDir := cwd
+    for curDir != "/" {
+        dirs, err := os.ReadDir(curDir)
+        if err != nil {
+            return "", err
+        }
+
+        if exists(dirs, ".git") {
+            return curDir, nil
+        }
+
+        curDir = path.Join(curDir, "..")
+    }
+    return "", ErrNoRepositoryFound{dir: cwd}
+}
+
+// Read Branch information from the config file
+// TODO if atleast one  branch is a necessity, then this function should return an error
+// Else, return an empty slice when there are no branches (this is current behavior)
+func getBranchesFromConfigIni (configIni *iniparse.IniFile) ([]Branch, error) {
+    branches := make([]Branch, 0)
+    for sections := range *configIni {
+        if strings.HasPrefix(sections, "branch") {
+            branches = append(branches, Branch {name: strings.Split(sections, " ")[1]})
+        }
+    }
+    return branches, nil
+}
+
+// Helper function to iterate over list of diretories/files and check for certain names
+func exists (entries []os.DirEntry, name string) bool {
+    for _, entry := range entries {
+        if entry.Name() == name {
+            return true
+        }
+    }
+    return false
+}
---
+++ repo/repo_test.go
@@ -0,0 +1,144 @@
+package repo
+
+import (
+    "testing"
+    "os"
+    "path"
+    "github.com/SimonMTaye/gitgo/iniparse"
+)
+
+
+
+func TestFindRepo(t *testing.T) {
+    tmpDir := t.TempDir()
+
+    nestedPath := path.Join(tmpDir, "nest1", "nest2", "nest3")
+    err := os.MkdirAll(nestedPath, DIR_FILEMODE)
+    if err != nil {
+        t.Fatalf("Error creating directories for testing\n")
+    }
+
+    err = os.Mkdir(path.Join(tmpDir, ".git"), DIR_FILEMODE)
+    if err != nil {
+        t.Fatalf("Error '.git' directory for testing\n")
+    }
+
+    repo1, err := FindRepo(tmpDir)
+    if repo1 != tmpDir {
+        t.Errorf("Wrong directory returned by FindRepo; Expected %s, Got %s\n", 
+        tmpDir, repo1)
+    }
+
+    if err != nil {
+        t.Errorf("Unexpected error returned by FindRepo:\n%s", err.Error())
+    }
+
+    repo2, err := FindRepo(nestedPath)
+    if repo2 != tmpDir {
+        t.Errorf("Wrong directory returned by FindRepo; Expected %s, Got %s\n", 
+        tmpDir, repo2)
+    }
+
+    if err != nil {
+        t.Errorf("Unexpected error returned by FindRepo:\n%s", err.Error())
+    }
+}
+
+func TestFindRepoOnNoRepo(t *testing.T) {
+    tmpDir := t.TempDir()
+
+    nestedPath := path.Join(tmpDir, "nest1", "nest2", "nest3")
+    err := os.MkdirAll(nestedPath, DIR_FILEMODE)
+    if err != nil {
+        t.Fatalf("Error creating directories for testing\n")
+    }
+
+    repo1, err := FindRepo(nestedPath)
+    if repo1 != "" {
+        t.Errorf("FindRepo returned an unexpected string:\n%s", repo1)
+    }
+
+    _, ok := err.(*ErrNoRepositoryFound)
+    if !ok {
+        if err != nil {
+            t.Errorf("FindRepo returned an unexpected error:\n%s", err.Error())
+        } else {
+            t.Errorf("Expected FindRepo to return an error, but nothing was returned")
+        }
+
+    }
+}
+
+
+func TestOpenRepo(t *testing.T) {
+    tmpDir := t.TempDir()
+    err := CreateRepo(tmpDir, "", "random")
+    if err != nil {
+        t.Fatalf("Unexpected error when creating a new repository for testing:\n%s",
+        err.Error())
+    }
+
+    repo, err := OpenRepo(tmpDir)
+    if err != nil {
+        t.Fatalf("Unexpected error when reading repository info for testing:\n%s",
+        err.Error())
+    }
+
+    if repo.repoPath != tmpDir || len(repo.branches) != 0 || repo.worktree != "random" {
+        t.Errorf("Repo object is different from expected.\n" +
+        "Expected repoPath: %s, Got: %s\n" +
+        "Expected worktree: %s, Got: %s\n" +
+        "Expected no repo branches, Got: %d\n", 
+        tmpDir, repo.repoPath, "random", repo.worktree, len(repo.branches))
+    }
+
+}
+
+
+func TestOpenRepoWithBranches(t *testing.T) {
+    tmpDir := t.TempDir()
+    err := CreateRepo(tmpDir, "", "")
+    if err != nil {
+        t.Fatalf("Unexpected error when creating a new repository for testing:\n%s",
+        err.Error())
+    }
+    // Open config file and modify it
+    configPath := path.Join(tmpDir, ".git", "config")
+    configFile, err := os.Open(configPath)
+    if err != nil {
+        t.Fatalf("Unexpected error when opening repository config file for testing:\n%s", 
+        err.Error())
+    }
+
+    configIni, err := iniparse.ParseIni(configFile)
+    if err != nil {
+        t.Fatalf("Unexpected error when parsing config file for testing:\n%s",err.Error())
+    }
+    configFile.Close()
+
+    configIni.SetProperty("branch \"main\"", "remote", "origin")
+    configIni.SetProperty("branch \"main\"", "merge", "refs/heads/main")
+
+    configFile, err = os.Create(configPath)
+    if err != nil {
+        t.Fatalf("Unexpected error when writing to repository config file for testing:\n%s", 
+        err.Error())
+    }
+
+    configFile.WriteString(configIni.String())
+    configFile.Close()
+
+    repo, err := OpenRepo(tmpDir)
+    if err != nil {
+        t.Fatalf("Unexpected error when reading repository info for testing:\n%s",
+        err.Error())
+    }
+    if repo.repoPath != tmpDir || len(repo.branches) != 1 || repo.worktree != tmpDir {
+        t.Errorf("Repo object is different from expected.\n" +
+        "Expected repoPath: %s, Got: %s\n" +
+        "Expected worktree: %s, Got: %s\n" +
+        "Expected repo branch: 1, Got: %d\n", 
+        tmpDir, repo.repoPath, tmpDir, repo.worktree, len(repo.branches))
+    }
+}
+
